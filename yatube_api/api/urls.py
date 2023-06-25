7b0ae67a537563909f6c96198a92f33133c421d5
Igor Chizh <igorchizh@MacBook-Pro-Igor.local>
2023-06-25 09:41:25 +0300

    Импорты по РЕР8.

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,7 +1,8 @@
-from api.views import CommentViewSet, GroupViewSet, PostViewSet
 from django.urls import include, path
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
+
+from api.views import CommentViewSet, GroupViewSet, PostViewSet
 
 router = DefaultRouter()
 router.register('posts',
