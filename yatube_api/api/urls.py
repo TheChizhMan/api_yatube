75c1e1e852fbbbd94b968a9431709d2c577c541a
Igor Chizh <igorchizh@MacBook-Pro-Igor.local>
2023-06-25 09:44:59 +0300

    Добавлен аргумент name.

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from rest_framework.authtoken import views
+from rest_framework.authtoken import views as auth_views
 from rest_framework.routers import DefaultRouter
 
 from api.views import CommentViewSet, GroupViewSet, PostViewSet
@@ -17,5 +18,5 @@ router.register('groups',
 
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('v1/api-token-auth/', views.obtain_auth_token),
+    path('v1/api-token-auth/', auth_views.obtain_auth_token, name='api_token_auth'),
 ]
