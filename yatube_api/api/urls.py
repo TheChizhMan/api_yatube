b8c45a0bf0faa9d751ad3775367f86641e22ab4e
Igor Chizh <igorchizh@MacBook-Pro-Igor.local>
2023-06-25 10:01:42 +0300

    PEP8.

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,5 +1,4 @@
 from django.urls import include, path
-from rest_framework.authtoken import views
 from rest_framework.authtoken import views as auth_views
 from rest_framework.routers import DefaultRouter
 
@@ -18,5 +17,7 @@ router.register('groups',
 
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('v1/api-token-auth/', auth_views.obtain_auth_token, name='api_token_auth'),
+    path('v1/api-token-auth/',
+         auth_views.obtain_auth_token,
+         name='api_token_auth'),
 ]
